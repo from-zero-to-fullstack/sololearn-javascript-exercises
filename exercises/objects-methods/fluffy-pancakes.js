226f9675a61c58e0baa893a7bc2a4eb67b72b83d
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-12 01:47:47 +0000

    organize branch

---
+++ exercises/objects-methods/fluffy-pancakes.js
@@ -0,0 +1,10 @@
+function main(input) {
+    var breakfasts = ['Cinnamon Doughnuts', 'Waffles', 'Granola', 'Chorizo Burrito', 'French Toast'];
+    var index = input;
+   
+    breakfasts[index] = "Fluffy Pancakes";
+    
+    console.log(breakfasts);
+    
+}
+main(input);
