2f5550921aef2fbe052f5f9a23d092aa147b9bc5
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-16 20:09:54 +0000

    Fix bug
    fix for statement

--- exercises/store-manager/store-manager.js
+++ exercises/store-manager/store-manager.js
@@ -2,26 +2,15 @@ function main() {
     var increase = parseInt(readLine(), 10);
     var prices = [98.99, 15.2, 20, 1026];
     //your code goes here
-    
-    //Opção 1
-    // for (let i = 0; i < prices.length; i = prices + increase) {
-    //    console.log(prices[i]);
-    //}
 
-    //Opção 2
-    // for (let i = 0; i < prices.length; i + increase) {
-    //    console.log(prices[i]);
-    //}
-
-    //Opção 3
-    // var newPrices = prices.length;
-    // for (let i = 0; i < prices.length; i + increase) {
-    //    console.log(prices[i]);
-    //}
+    for (let i = 0; i < prices.length; i++) {
+        prices[i] += increase;
+    }
+    console.log(prices);
 }
 
 //DOCUMENTAÇÃO:
-var myStringArray = ["Hello","World"];
+var myStringArray = ["Hello", "World"];
 var arrayLength = myStringArray.length;
 for (var i = 0; i < arrayLength; i++) {
     console.log(myStringArray[i]);
