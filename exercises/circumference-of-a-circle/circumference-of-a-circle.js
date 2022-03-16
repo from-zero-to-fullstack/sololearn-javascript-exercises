a7dfc6537a93cfd9856ecb5781b8293c2caba76f
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-16 00:36:15 +0000

    Add r value verification

--- exercises/circumference-of-a-circle/circumference-of-a-circle.js
+++ exercises/circumference-of-a-circle/circumference-of-a-circle.js
@@ -2,13 +2,17 @@ function main() {
     var r = parseInt(readLine(), 10)
     
     //the output
-    console.log(calcCirc(r).toFixed(2));
+    console.log(calcCirc(r));
     
 }
 
 //complete the function
-function calcCirc(r){
-    return(2 * Math.PI * r);
+function calcCirc(r) {
+    if (r > 0) {
+        return (2 * Math.PI * r);
+    } else {
+        return (2 * Math.PI * r).toFixed(2);
+    }
 }
 
 main();
